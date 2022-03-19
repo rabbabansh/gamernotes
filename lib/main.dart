4fe28e6947125bc1f91c970b547780813ae14100
rabbabansh <ansh28babbar@gmail.com>
2022-03-19 06:49:14 +0530

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gamernotes/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:gamernotes/views/login_view.dart';
 import 'package:gamernotes/views/register_view.dart';
+import 'package:gamernotes/views/verify_email_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +35,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified.');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
             // print(user);
             // if (user?.emailVerified ?? false) {
             //   return const Text('Done');
